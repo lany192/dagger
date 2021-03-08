8f81c5411b3d6e041de70c44ebb722c4e40312c2
Lany192 <lany192@qq.com>
2021-03-08 10:54:04 +0800

    update

--- sample/src/main/java/com/github/lany192/dagger/sample/MainActivity.java
+++ sample/src/main/java/com/github/lany192/dagger/sample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.github.lany192.dagger.sample;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import dagger.android.support.DaggerAppCompatActivity;
@@ -10,5 +11,7 @@ public class MainActivity extends DaggerAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, SettingsActivity.class)));
+        findViewById(R.id.button2).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AboutActivity.class)));
     }
 }
