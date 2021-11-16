05ce87c3918d3529584cb4ce6ca730dc7a78d124
lany <lany192@qq.com>
2021-11-16 21:46:32 +0800

    增加对ARouter的支持

--- sample/src/main/java/com/github/lany192/dagger/sample/AboutActivity.java
+++ sample/src/main/java/com/github/lany192/dagger/sample/AboutActivity.java
@@ -1,16 +1,23 @@
 package com.github.lany192.dagger.sample;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+
+import javax.inject.Inject;
+
 import dagger.android.support.DaggerAppCompatActivity;
 
 @Route(path = "/app/about")
 public class AboutActivity extends DaggerAppCompatActivity {
+    @Inject
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ((TextView) findViewById(R.id.show)).setText(user.toString());
     }
 }
--- sample/src/main/java/com/github/lany192/dagger/sample/SettingsActivity.java
+++ sample/src/main/java/com/github/lany192/dagger/sample/SettingsActivity.java
@@ -1,17 +1,23 @@
 package com.github.lany192.dagger.sample;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.github.lany192.dagger.annotation.Dagger;
+
+import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
 @Dagger
 public class SettingsActivity extends DaggerAppCompatActivity {
+    @Inject
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ((TextView) findViewById(R.id.show)).setText(user.toString());
     }
 }
