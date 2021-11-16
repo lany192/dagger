4ad5f474337b211c88173ccac748c37720ae70f0
lany <lany192@qq.com>
2021-11-16 21:12:13 +0800

    update

--- sample/src/main/java/com/github/lany192/dagger/sample/MainActivity.java
+++ sample/src/main/java/com/github/lany192/dagger/sample/MainActivity.java
@@ -2,13 +2,18 @@ package com.github.lany192.dagger.sample;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.github.lany192.dagger.annotation.Dagger;
+
+import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
 @Dagger
 public class MainActivity extends DaggerAppCompatActivity {
+    @Inject
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,6 @@ public class MainActivity extends DaggerAppCompatActivity {
         setContentView(R.layout.activity_main);
         findViewById(R.id.button).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, SettingsActivity.class)));
         findViewById(R.id.button2).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AboutActivity.class)));
+        ((TextView) findViewById(R.id.show)).setText(user.toString());
     }
 }
--- sample/src/main/java/com/github/lany192/dagger/sample/di/module/CustomModule.java
+++ sample/src/main/java/com/github/lany192/dagger/sample/di/module/CustomModule.java
@@ -1,8 +1,18 @@
 package com.github.lany192.dagger.sample.di.module;
 
+import com.github.lany192.dagger.sample.User;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 
 @Module
-public abstract class CustomModule {
+public class CustomModule {
 
+    @Singleton
+    @Provides
+    User provideUser() {
+        return new User(1,"Lany");
+    }
 }
